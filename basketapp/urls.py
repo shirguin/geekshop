fa2b3f089609dd20604ba4fe79df6971a38eb6ff
Shirguin <shirguin@yandex.ru>
2021-12-05 16:04:04 +0300

    Домашнее задание к 6 уроку

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', basketapp.basket, name='basket'),
     path('add/<int:pk>/', basketapp.basket_add, name='basket_add'),
     path('remove/<int:pk>/', basketapp.basket_remove, name='basket_remove'),
-
+    path('edit/<int:pk>/<quantity>/', basketapp.basket_edit, name='basket_edit'),
 ]
