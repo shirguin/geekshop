630edf265c24d5a11d42553a53516f011c081e0a
shirguin <shirguin@yandex.ru>
2022-01-21 15:49:50 +0300

    Добавил метод select_related() в контроллер редактирования заказа

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -82,7 +82,9 @@ class OrderUpdateView(UpdateView):
         if self.request.method == 'POST':
             formset = OrderFormSet(self.request.POST, instance=self.object)
         else:
-            formset = OrderFormSet(instance=self.object)
+            queryset = self.object.orderitems.select_related()
+            formset = OrderFormSet(instance=self.object, queryset=queryset)
+            # formset = OrderFormSet(instance=self.object)
             for form in formset.forms:
                 if form.instance.pk:
                     form.initial['price'] = form.instance.product.price
