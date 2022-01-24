9e0056a5270c7ec8efeffc04bf44ada352536a0f
Shirguin <shirguin@yandex.ru>
2022-01-24 06:47:37 +0300

    добавил метод get_items_cached() c декоратором @cached_property

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils.functional import cached_property
 
 from mainapp.models import Product
 
@@ -27,15 +28,21 @@ class Basket(models.Model):
     def product_cost(self):
         return self.product.price * self.quantity
 
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
     @property
     def total_quantity(self):
-        items = Basket.objects.filter(user=self.user)
+        # items = Basket.objects.filter(user=self.user)
+        items = self.get_items_cached
         _total_quantity = sum(list(map(lambda x: x.quantity, items)))
         return _total_quantity
 
     @property
     def total_cost(self):
-        items = Basket.objects.filter(user=self.user)
+        # items = Basket.objects.filter(user=self.user)
+        items = self.get_items_cached
         _total_cost = sum(list(map(lambda x: x.product_cost, items)))
         return _total_cost
 
